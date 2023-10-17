641f6943bebed450c99c9eaba58ff605967bae10
Jonas J <jonas.jiunchetti@outlook.com>
2023-10-17 15:08:03 -0300

    First commit

---
+++ src/test/java/dio/api/SantanderAccountApp2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderAccountApp2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
